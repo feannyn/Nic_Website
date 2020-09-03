9a476f5e475614cf3a803c7167fefdf7b65f915e
Nic Feanny <feannyn@gmail.com>
2020-09-03 15:22:03 -0400

    images

---
+++ src/res/imagePaths.js
@@ -0,0 +1,7 @@
+export { default as dota2day } from './images/Dota2Day/Dota2Day_Comb.png';
+export { default as Neverzone } from './images/Slackbot_Img/NeverZone_Type_Comb.png';
+export { default as Fun_Img_1 } from './images/Fun_Imgs/Fun_Img_1.png';
+export { default as Fun_Img_2 } from './images/Fun_Imgs/Fun_Img_2.png';
+export { default as Fun_Img_3 } from './images/Fun_Imgs/Fun_Img_3.png';
+export { default as Fun_Img_4 } from './images/Fun_Imgs/Fun_Img_4.png';
+export { default as Core_Logo } from './images/Core_Virtru_Logo/logo-front-white.png';
---
+++ src/res/strings.js
@@ -0,0 +1,26 @@
+const STRING_CONSTANTS = {
+  WELCOME: 'Welcome!',
+  HELLO: ['Hello, and welcome to my website! The goal of this website is to \n',
+    'apply skills which I have acquired via my internships and my own free \n',
+    'time outside my classes. This website was created with React in conjunction with the \n',
+    'Materialize CSS framework.'],
+  INTRO: ['My name is Nicholas Feanny. I am majoring in Computer Science at Florida State University.\n',
+    'During my free time, I try my best to expose myself to new hobbies and skills!\n',
+    'I enjoying running, mountain biking, crossfit, and playing video games with my friends during\n',
+    'my free time.'],
+  HOW_I_GOT_HERE: ['In 2015, I graduated with my first undergraduate degree in political science\n',
+    'while also working at a law firm as a project assistant for three years. At that moment,\n',
+    'my intent was to go to law school and become an attorney, with the goal of making an impact \n',
+    'on society. However, while this made sense logically given my experience at that time, I discovered \n',
+    'a few months into law school that being an attorney was not for me; so I withdrew. I decided the best \n',
+    'course of action was to reinvent myself and follow a path which paralleled my actual interests rather \n',
+    'than my acquired skillset. I had no prior experience in programming or software development, but I had \n',
+    'always gravitated towards tech and decided to take the risk and pur-sue it. I went to a coding bootcamp \n',
+    'prior to coming to FSU and from that moment I knew that software development was what I wanted to do and \n',
+    'intend on doing. Out of the gate, I felt that the biggest decision in my life was leaving law school \n',
+    'because my experiences culminated to becoming an attorney. However, the best decision I ever made was \n',
+    'to pursue my interests regardless of the outcome because in the end that is what matters.']
+
+};
+
+export default STRING_CONSTANTS;
